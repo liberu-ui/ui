93247713e492535e4e042b1607f10195e16bcf31
Curtis Delicata <curtis.delicata@outlook.com>
2023-10-06 16:15:23 +0000

    Fix

--- src/bulma/components/AppFooter.vue
+++ src/bulma/components/AppFooter.vue
@@ -35,7 +35,6 @@
             </div>
             <div class="level">
                 <div class="level-item">
-                    </a>
                 </div>
             </div>
             <div>
