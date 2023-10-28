5b56a1a6a46e25b1e48e2068da88773bf40140cf
aronmakiika <aronmakiika@gmail.com>
2023-10-28 20:05:51 +0300

    feat: build and start the app

--- src/bulma/components/menu/Sidebar.vue
+++ src/bulma/components/menu/Sidebar.vue
@@ -1,43 +1,42 @@
 <template>
-    <vue-aside class="sidebar no-scrollbars"
-        :class="$attrs.class">
-        <p class="menu-label is-bold has-text-centered">
-            {{ i18n("Menu") }}
-        </p>
-        <core-sidebar>
-            <template #default="{ menus }">
-                <menus v-bind="$attrs"
-                    :menus="menus"/>
-            </template>
-        </core-sidebar>
-    </vue-aside>
+  <div>hello</div>
+  <!-- <vue-aside class="sidebar no-scrollbars" :class="$attrs.class">
+    <p class="menu-label is-bold has-text-centered">
+      {{ i18n("Menu") }}
+    </p>
+    <core-sidebar>
+      <template #default="{ menus }">
+        <menus v-bind="$attrs" :menus="menus" />
+      </template>
+    </core-sidebar>
+  </vue-aside> -->
 </template>
 
 <script>
-import Menus from '@liberu-ui/menus/src/bulma/components/menu/Menus.vue';
-import CoreSidebar from '../../../core/components/menu/Sidebar.vue';
-import VueAside from '../VueAside.vue';
+import Menus from "@liberu-ui/menus/src/bulma/components/menu/Menus.vue";
+import CoreSidebar from "../../../core/components/menu/Sidebar.vue";
+import VueAside from "../VueAside.vue";
 
 export default {
-    name: 'Sidebar',
+  name: "Sidebar",
 
-    components: { CoreSidebar, Menus, VueAside },
+  components: { CoreSidebar, Menus, VueAside },
 
-    inject: ['i18n'],
+  inject: ["i18n"],
 
-    inheritAttrs: false,
+  inheritAttrs: false,
 };
 </script>
 
 <style lang="scss">
-    @import '@liberu-ui/themes/bulma/variables';
+@import "@liberu-ui/themes/bulma/variables";
 
-    .aside.sidebar {
-        overflow-y: auto;
-        transition: width .5s;
+.aside.sidebar {
+  overflow-y: auto;
+  transition: width 0.5s;
 
-        &.is-collapsed {
-            width: $sidebar-collapsed-width;
-        }
-    }
+  &.is-collapsed {
+    width: $sidebar-collapsed-width;
+  }
+}
 </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>home page</div>
+  <Sidebar />
+</template>
+
+<script setup>
+import Sidebar from "../bulma/layouts/Home.vue";
+</script>
+
+<style scoped></style>
